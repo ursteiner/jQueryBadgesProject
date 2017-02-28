b865c3b876da9a1e05668f9b2a9474eacab227ec
ursteiner <c.mueller@topdesk.com>
2017-02-28 14:54:56 +0100

    implemented initial ajax call generating badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1122673.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      handleBadges(response.courses.completed);
+    }
+  });
 
-  // your code will go here
+  function handleBadges(badges) {
+    $.each(badges, function(index, item) {
+      var batchDiv = '<div class="course">';
+      batchDiv += "<h3>" + item.title + "</h3>";
+      batchDiv += '<img src="' + item.badge + '">';
+      batchDiv += '<a href="' + item.url + '" target="_blank" class="btn btn-primary">See Course</a>';
+      batchDiv += "</div>";
 
+      $('#badges').append(batchDiv);
+    });
+  }
 });
